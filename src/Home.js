4bc918d66870361a9dd0aaea6252f82fa9da2591
princegobah <babynamek@gmail.com>
2023-12-26 15:04:08 -0500

    testing

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ const [testState, setTestState]=useState(false)
 
 return(
     <div>
-        <h1>This sucks</h1>
+        <h1></h1>
         <h2 onClick={(e)=>{setTestState(true)}}>Todo<input type="checkbox"/></h2>
        {testState?<p>true</p>:<p>false</p>}
 
